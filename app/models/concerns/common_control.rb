65c8e87c39f451fd61a15fcf7905f4eb5a4c48c9
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-08-29 21:17:34 -0400

    Refactored common_control. Modified the jbuilder partial 'app/views/reports/events/_event.json.jbuilder:5', by adding uniqueness by email in the total amount of speakers

--- app/models/concerns/common_control.rb
+++ app/models/concerns/common_control.rb
@@ -1,22 +1,14 @@
 module CommonControl
   extend ActiveSupport::Concern
 
-  included do
-    before_action :update_api_pagination,
-                  :update_sorting
-  end
-
-  # Update pagination variables based on parameters
+  # Updates the pagination variables based on parameters
   def update_api_pagination
-    # @per_page = params[:per_page] || Application.config.common_batch_size
-    # @page = params[:page] || 1
-    # @limit = (l = params[:limit].to_i) == 0 ? 100 : l
     @limit = (l = params[:limit].to_i) == 0 ? Rails.application.config.common_batch_size : l
     @page = (p = params[:page].to_i) == 0 ? 1 : p
-    @offset = ((@page -1) * @limit)
+    @offset = (@page - 1) * @limit
   end
 
-  # Update sorting variables based on parameters
+  # Updates the sorting variables based on parameters
   def update_sorting
     @sort_column = params[:sort_column] || 'created_at'
     @sort_direction = params[:sort_direction] || 'desc'
